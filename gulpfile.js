82c657fac2bba5a211e1ef527ae7cca90280f84a
iyke <nderiony95@gmail.com>
2017-05-24 18:30:06 +0300

    add js file

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// var gulp = require("gulp");
+// var browserify = require('browserify');
+// var source = require('vinyl-source-stream');
+// var lib = require
+//
+//
+// gulp.task('jsBrowserify', function() {
+//   return browserify({ entries: ['./js/alarm.js'] })
+//   .bundle()
+//   .pipe(source('app.js'))
+//   .pipe(gulp.dest('./build/js'));
+//
+// });
--- js/alarm.js
+++ js/alarm.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-  $('#time').text(moment());
+//$(document).ready(function(){
+//$('#time').text(moment().format("LT "));
+//});
+var currentTime = function() {
+  moment().format("LTS");
+}
+
+$(document).ready(function() {
+  $('#time').text(moment().format("LT "));
+  $('#alarm-form').submit(function() {
+    var alarm = $('#tim').val();
+    $('#alarm-set').text(alarm);
+    event.preventDefault()
+
+  });
 });
+
+// backend
+var alarm = function(tim){
+  if (currentTime === "#time") {
+    alert("Wake up")
+  }
+
+}
